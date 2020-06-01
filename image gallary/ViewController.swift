c65797edd5d80cc58e4360eabcd21f4e37b48f3f
Moutaz_Hegazy <moutaz_hegazy@Moutaz-Hegazys-Mac.local>
2020-06-01 06:35:08 -0700

    Initial Commit

---
+++ image gallary/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  image gallary
+//
+//  Created by Moutaz_Hegazy on 6/1/20.
+//  Copyright © 2020 Moutaz_Hegazy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
